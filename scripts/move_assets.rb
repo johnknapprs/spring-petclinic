46e80cd5a45c14bc3c545a0adecac33a3f66b95e
John Knapp <knappj2@gmail.com>
2019-09-12 05:28:36 -0400

    improving move script

--- scripts/move_assets.rb
+++ scripts/move_assets.rb
@@ -2,19 +2,35 @@
 
 require 'fileutils'
 
-project_root = File.expand_path('.')
+project_root     = File.expand_path('.')
+jenkinsfile_path = File.join(project_root, 'Jenkinsfile')
+settings_path    = File.join(project_root, 'settings.xml')
+
 update_files = case ARGV.first.to_s.strip
                when /base/
+                 [jenkinsfile_path, settings_path].each do |file|
+                   if File.exist?(file)
+                     puts "Removing #{file}"
+                     FileUtils.rm_f(file)
+                   end
+                 end
+
+                 Dir['scripts/demo_base/*'].map { |f| File.expand_path(f) }
+               when /build/
                  Dir['scripts/demo_build/*'].map { |f| File.expand_path(f) }
                when /deploy/
                  Dir['scripts/demo_deploy/*'].map { |f| File.expand_path(f) }
-               when /creds/
+               when /secrets/
                  Dir['scripts/demo_secrets/*'].map { |f| File.expand_path(f) }
+               when /upstream/
+                 Dir['scripts/demo_upstream/*'].map { |f| File.expand_path(f) }
+               when /final/
+                 Dir['scripts/demo_zfinal/*'].map { |f| File.expand_path(f) }
                else
                  raise 'Unable to find matching configurations'
                end
 
-puts 'Updating Jenkinsfile'
+puts 'Updating files'
 
 update_files.each do |file|
   FileUtils.cp(file, project_root)
